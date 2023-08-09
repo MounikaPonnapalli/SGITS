78533180a0e1bf923d63bf93735476c6b960a9a9
Mounika <mouniponnapalli16@gmail.com>
2023-08-09 18:25:54 +0530

    9-8-2023

---
+++ RoundRobin.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+class Process 
+{
+    String name;
+    int bTime;
+    int rTime;
+}
+public class RoundRobin
+{
+    public static void main(String[] args)
+    {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter the number of processes:");
+        int num=input.nextInt();
+        System.out.print("Enter the Time Quantum:");
+        int tq=input.nextInt();
+        Process[] pro=new Process[num];
+        for(int i=0;i<num;i++)
+        {
+            pro[i]=new Process();
+            System.out.print("Enter burst time for p"+(i+1));
+            pro[i].bTime=input.nextInt();
+            pro[i].rTime=pro[i].bTime;
+        }
+        int cT=0;
+        int cpComplete=0;
+        while(cpComplete<num)
+        {
+            for(int i=0;i<num;i++)
+            {
+                Process currentProcess=pro[i];
+                if(currentProcess.rTime>0)
+                {
+                    int executionTime=Math.min(tq,currentProcess.rTime);
+                    System.out.println("Executing process "+(i+1)+" for "+executionTime);
+                    cT+=executionTime;
+                    currentProcess.rTime-=executionTime;
+                    if(currentProcess.rTime==0)
+                    {
+                        System.out.println("Process"+(i+1)+"is complete");
+                        cpComplete++;
+                    }
+                }
+            }
+        }
+        System.out.println("gantt chart:");
+        for(int i=0;i<num;i++)
+        {
+            System.out.print("P"+pro[i]+" ");
+        }
+    }
+
+}
