e89217cdd8ec503497deaa26fde4154cadb06a3d
Mounika <mouniponnapalli16@gmail.com>
2023-08-16 18:10:03 +0530

    Libraray Task 16-8-2023

---
+++ Library/src/main/java/com/example/demo/controller/LibraryController.java
@@ -0,0 +1,74 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Book;
+import com.example.demo.service.BookService;
+
+@RestController
+@RequestMapping("/library")
+public class LibraryController {
+    @Autowired
+    private BookService bookService;
+
+    
+    @PostMapping("/addBook")
+    public void addBook(@RequestBody Book book) {
+        bookService.saveBook(book);
+    }
+    
+    @GetMapping("/books")
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/available")
+    public List<Book> getAvailableBooks() {
+        return bookService.getAvailableBooks();
+    }
+
+    @GetMapping("/book")
+    public ResponseEntity<Book> getBookById(@RequestParam("id") Long id) {
+        Book book = bookService.getBookById(id);
+        if (book != null) {
+            return ResponseEntity.ok(book);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @PutMapping("/update")
+    public ResponseEntity<Book> updateBook(@RequestParam("id") Long id, @RequestBody Book updatedBook) {
+        Book existingBook = bookService.getBookById(id);
+        
+        if(existingBook != null) 
+        {
+            existingBook.setTitle(updatedBook.getTitle());
+            existingBook.setAuthor(updatedBook.getAuthor());
+            existingBook.setAvailable(updatedBook.isAvailable());
+
+            bookService.saveBook(existingBook);
+
+            return ResponseEntity.ok(existingBook);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+    @DeleteMapping("/deleteBook")
+    public void deleteBook(@RequestParam("id") Long id) {
+        bookService.deleteBook(id);
+    }
+}
---
+++ Library/src/main/java/com/example/demo/service/BookService.java
@@ -0,0 +1,45 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Book;
+import com.example.demo.repository.BookRepository;
+
+@Service
+public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public List<Book> getAvailableBooks() {
+        return bookRepository.findByAvailable(true);
+    }
+
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    public void saveBook(Book book) {
+        bookRepository.save(book);
+    }
+
+    public void updateBook(Long id, Book updatedBook) {
+        Book book = bookRepository.findById(id).orElse(null);
+        if (book != null) {
+            book.setTitle(updatedBook.getTitle());
+            book.setAuthor(updatedBook.getAuthor());
+            book.setAvailable(updatedBook.isAvailable());
+            bookRepository.save(book);
+        }
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
